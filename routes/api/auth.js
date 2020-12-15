20ed0727f2ab8944ea2c6dd3e4130e4c65ad7b0e
Osman Başar Akkaya <akkayabasar@gmail.com>
2020-12-15 13:25:14 +0300

    Login & register apis created

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -31,6 +31,62 @@ router.get('/', auth, async (req, res) => {
  * @description Authenticate user & get token (Login)
  * @access      Public
  */
-router.post('/', [], async (req, res) => {});
+router.post(
+  '/',
+  [
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Please enter a password').exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid credentials' }] });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid credentials' }] });
+      }
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) {
+            logger.error(err);
+            throw err;
+          }
+
+          res.json({ token });
+        }
+      );
+    } catch (error) {
+      logger.error(error.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 module.exports = router;
--- routes/api/users.js
+++ routes/api/users.js
@@ -6,23 +6,86 @@ const jwt = require('jsonwebtoken');
 const config = require('config');
 const { check, validationResult } = require('express-validator');
 
-const auth = require('../../middleware/auth');
 const logger = require('../../util/logger');
 
 const User = require('../../models/User');
-
-/**
- * @route       GET api/users
- * @description Get all users
- * @access      Private
- */
-router.get('/', auth, async (req, res) => {});
 
 /**
  * @route       POST api/users
  * @description Register User (Signup)
  * @access      Public
  */
-router.post('/', [], async (req, res) => {});
+router.post(
+  '/',
+  [
+    check('name', 'Name is reuired').not().isEmpty(),
+    check('email', 'Email is required').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+      'password',
+      'Please enter a password with minimum 6, maximum 20 characters'
+    ).isLength({ min: 6, max: 20 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (user) {
+        logger.error('User already exists');
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'User already exists' }] });
+      }
+
+      const avatar = gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm',
+      });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
+
+      // encrypt password
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      const payload = {
+        user: { id: user.id },
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) {
+            logger.error(err);
+            throw err;
+          }
+
+          res.json({ token });
+        }
+      );
+    } catch (error) {
+      logger.error(error.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 module.exports = router;
